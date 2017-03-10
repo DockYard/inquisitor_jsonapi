3b120caa7615abbc27ccdc9c470c55e67e6a4e39
Brian Cardarella <brian@dockyard.com>
2017-03-10 17:32:00 -0500

    Fix per-page offset

--- test/jsonapi/page_test.exs
+++ test/jsonapi/page_test.exs
@@ -10,7 +10,14 @@ defmodule Inquisitor.JsonApi.PageTest do
   test "supports `page[number]` and `page[size]`" do
     q = Base.build_query(User, %{"page" => %{"number" => "1", "size" => "10"}})
     assert to_sql(q) == {~s{SELECT u0."id", u0."name", u0."age" FROM "users" AS u0 LIMIT $1 OFFSET $2}, [10, 0]}
+  end
 
+  test "supports `page[number]` and `page[size]` with multiple pages" do
+    q = Base.build_query(User, %{"page" => %{"number" => "2", "size" => "10"}})
+    assert to_sql(q) == {~s{SELECT u0."id", u0."name", u0."age" FROM "users" AS u0 LIMIT $1 OFFSET $2}, [10, 10]}
+  end
+
+  test "supports `page[number]` and `page[size]` non-strings" do
     q = Base.build_query(User, %{"page" => %{"number" => 1, "size" => 10}})
     assert to_sql(q) == {~s{SELECT u0."id", u0."name", u0."age" FROM "users" AS u0 LIMIT $1 OFFSET $2}, [10, 0]}
   end
