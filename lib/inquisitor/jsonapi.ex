965c98e42227b3afc8c3e2a898cbdc8884753f29
Brian Cardarella <brian@dockyard.com>
2017-03-25 19:09:05 -0400

    JsonApi module

--- lib/inquisitor/jsonapi.ex
+++ lib/inquisitor/jsonapi.ex
@@ -1,3 +1,10 @@
 defmodule Inquisitor.JsonApi do
-
+  defmacro __using__(_opts) do
+    quote do
+      use Inquisitor
+      use Inquisitor.JsonApi.Filter
+      use Inquisitor.JsonApi.Page
+      use Inquisitor.JsonApi.Sort
+    end
+  end
 end
